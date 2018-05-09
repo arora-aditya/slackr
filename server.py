cdb94c639224ab7e315f365d903a1de9694de0f4
Aditya Arora <10962267+arora-aditya@users.noreply.github.com>
2018-05-08 23:11:46 -0400

    added handler for SIGINT and global utils

--- server.py
+++ server.py
@@ -1,5 +1,9 @@
 import socket
 import sys
+from utils import  MESSAGE_LENGTH, SERVER_INVALID_CONTROL_MESSAGE, SERVER_NO_CHANNEL_EXISTS,\
+ SERVER_JOIN_REQUIRES_ARGUMENT, SERVER_CLIENT_JOINED_CHANNEL,\
+   SERVER_CLIENT_LEFT_CHANNEL, SERVER_CHANNEL_EXISTS,\
+    SERVER_CREATE_REQUIRES_ARGUMENT, SERVER_CLIENT_NOT_IN_CHANNEL
 
 args = sys.argv
 
@@ -11,18 +15,29 @@ class BasicServer(object):
         self.socket.listen(5)
 
     def start(self):
-        while True:
-            (new_socket, address) = self.socket.accept()
-            msg = new_socket.recv(1024)
-            tmp = msg
-            while tmp:
-                tmp = new_socket.recv(1024)
-                msg += tmp
-            print(msg)
+        try:
+            while True:
+                (new_socket, address) = self.socket.accept()
+                msg = new_socket.recv(1024)
+                tmp = msg
+                while tmp:
+                    tmp = new_socket.recv(1024)
+                    msg += tmp
+                print(msg.decode())
+        except KeyboardInterrupt:
+            print()
+            print("Caught interrupt, stopping server")
+            sys.exit()
 
 
-if len(args) != 2:
-    print("Please supply a port.")
-    sys.exit()
-server = BasicServer(args[1])
-server.start()
+if __name__ == '__main__':
+    if len(args) <= 2:
+        port = 8000
+    elif len(args) == 2:
+        port = args[1]
+    else:
+        port = args[1]
+        print("Ignoring other arguments, starting server on port {}".format(str(port)))
+    print('\x1b[1;32;40m' + 'Starting server, Ctrl+C to stop' + '\x1b[0m')
+    server = BasicServer(port)
+    server.start()
---
+++ utils.py
@@ -0,0 +1,16 @@
+MESSAGE_LENGTH = 200
+CLIENT_CANNOT_CONNECT = "Unable to connect to {0}:{1}"
+CLIENT_SERVER_DISCONNECTED = "Server at {0}:{1} has disconnected"
+CLIENT_MESSAGE_PREFIX = "[Me] "
+CLIENT_WIPE_ME = "\r    "
+SERVER_INVALID_CONTROL_MESSAGE = \
+  "{0} is not a valid control message. Valid messages are /create, /list, and /join."
+SERVER_NO_CHANNEL_EXISTS = "No channel named {0} exists. Try '/create {0}'?"
+SERVER_JOIN_REQUIRES_ARGUMENT = "/join command must be followed by the name of a channel to join."
+SERVER_CLIENT_JOINED_CHANNEL = "{0} has joined"
+SERVER_CLIENT_LEFT_CHANNEL = "{0} has left"
+SERVER_CHANNEL_EXISTS = "Room {0} already exists, so cannot be created."
+SERVER_CREATE_REQUIRES_ARGUMENT = \
+  "/create command must be followed by the name of a channel to create"
+SERVER_CLIENT_NOT_IN_CHANNEL = \
+  "Not currently in any channel. Must join a channel before sending messages."
